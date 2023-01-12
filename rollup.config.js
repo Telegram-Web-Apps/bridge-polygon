7bb5debf3cf9aa83eff1257b964b16e40403e25f
Vladislav Kibenko <wolfram.deus@gmail.com>
2023-01-12 06:47:58 +0500

    Configure build script

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,10 @@ import typescript from '@rollup/plugin-typescript';
 import {terser} from 'rollup-plugin-terser';
 import {defineConfig} from 'rollup';
 import {nodeResolve} from '@rollup/plugin-node-resolve';
+import html from '@rollup/plugin-html';
 
-export default // Browser.
-defineConfig({
+export default defineConfig({
   input: 'src/index.ts',
   output: {file: './build/bundle.js', format: 'iife'},
-  plugins: [typescript(), nodeResolve(), terser()],
+  plugins: [html(), typescript(), nodeResolve(), terser()],
 });
